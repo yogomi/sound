16bd4518e3091f550c313ee832f9e1430a90d471
Makoto Yano <yan133@gmail.com>
2013-07-22 03:52:07 +0900

    キューを使った出力のテスト成功

--- sound.cpp
+++ sound.cpp
@@ -5,6 +5,23 @@
 #include <AL/alut.h>
 using namespace std;
 
+#define BUFFER_SIZE 3
+#define rec_f 44100
+
+struct BUFFER{
+    ALuint buffer[BUFFER_SIZE];
+    ALint status[BUFFER_SIZE];
+};
+
+void initialize_buffer(struct BUFFER &buffer)
+{
+    alGenBuffers(BUFFER_SIZE, buffer.buffer);
+    for(int i=0; i<BUFFER_SIZE; i++){
+        buffer.status[i] = 0;
+    }
+    return;
+}
+
 void errorCheck()
 {
     switch(alGetError()){
@@ -60,21 +77,66 @@ void debugLine()
     return;
 }
 
+ALint get_useable_buffer_number(struct BUFFER &buffer)
+{
+    for(ALint i=0; i<BUFFER_SIZE; i++){
+        cout << buffer.status[i] << endl;
+        if(buffer.status[i] == 0){
+            return i;
+        }
+    }
+    return -1;
+}
+
+void que_buffer(ALuint source
+        , struct BUFFER &buffer
+        , ALvoid *data
+        , ALint data_size)
+{
+
+    ALint buffer_number = get_useable_buffer_number(buffer);
+    if(buffer_number < 0){
+        cout << "No useable buffer." << endl;
+        return;
+    }
+    alBufferData(buffer.buffer[buffer_number]
+                , AL_FORMAT_MONO16
+                , data
+                , data_size
+                , rec_f);
+    alSourceQueueBuffers(source, 1, &buffer.buffer[buffer_number]);
+    buffer.status[buffer_number] = 1;
+}
+
+void deque_buffer(ALuint source, struct BUFFER &buffer)
+{
+    ALint used_buffer;
+    alGetSourcei(source, AL_BUFFERS_PROCESSED, &used_buffer);
+    cout << "used buffer = " << used_buffer << endl;
+    if(used_buffer == 0) return;
+
+    alSourceUnqueueBuffers(source, 1, &buffer.buffer[used_buffer]);
+    buffer.status[used_buffer] = 0;
+    deque_buffer(source, buffer);
+    return;
+}
+
 int main(int argc, char** argv)
 {
     alutInit(&argc, argv);
-    ALuint buffer, source;
+    ALuint source;
+    struct BUFFER buffer;
     ALshort buff[22050];
     ALint sample, status;
 
     ALCdevice* rec_device;
-    ALCuint rec_f = 44100;
     ALCsizei bufsize = 1024;
     const ALchar *pDeviceList = alcGetString(NULL, ALC_CAPTURE_DEVICE_SPECIFIER);
     cout << "device list" << endl;
     while(*pDeviceList){
         cout << pDeviceList << endl;
         pDeviceList += strlen(pDeviceList) +1;
+
     }
     rec_device = alcCaptureOpenDevice(NULL, rec_f
             , AL_FORMAT_MONO16
@@ -82,7 +144,7 @@ int main(int argc, char** argv)
     if(alGetError() != AL_NO_ERROR){
         return 0;
     }
-    alGenBuffers(1, &buffer);
+    initialize_buffer(buffer);
     alGenSources(1, &source);
     alcCaptureStart(rec_device);
     while(true){
@@ -95,27 +157,14 @@ int main(int argc, char** argv)
                 , sample);
         alGetSourcei(source, AL_SOURCE_STATE, &status);
         debugLine();
-        if(status == AL_INITIAL || status == AL_STOPPED){
-            alSourcei(source, AL_BUFFER, AL_NONE);
-        /*
-            for(int i=0; i<sample; i++){
-                cout << buff[i] << " ";
-            }
-            */
-            alBufferData(buffer
-                    , AL_FORMAT_MONO16
-                    , buff
-                    , sample * 2
-                    , rec_f);
-            alSourcei(source, AL_BUFFER, buffer);
-            cout << "set buffer" << endl;
-        }
+        deque_buffer(source, buffer);
+        que_buffer(source, buffer, buff, sample*2);
         alGetSourcei(source, AL_SOURCE_STATE, &status);
         if(status != AL_PLAYING){
             alSourcePlay(source);
         }
         errorCheck();
-        alutSleep(0.04);
+        alutSleep(0.05);
     }
     alcCaptureStop(rec_device);
     alcCaptureCloseDevice(rec_device);
@@ -124,11 +173,11 @@ int main(int argc, char** argv)
     for(int i=0; i<freq/Hz; ++i){
         data[i] = 32767 * sin(i * 3.14159 * 2 * Hz / freq);
     }
-    alBufferData(buffer
+    alBufferData(buffer.buffer[0]
             , AL_FORMAT_MONO16
             , data
             , freq/Hz * sizeof(ALshort), freq);
-    alSourcei(source, AL_BUFFER, buffer);
+    alSourcei(source, AL_BUFFER, buffer.buffer[0]);
     alSourcei(source, AL_LOOPING, AL_TRUE);
     alSourcePlay(source);
     alutSleep(2);
